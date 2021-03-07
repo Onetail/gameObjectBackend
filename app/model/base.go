bc80ae1f2999e5c6855ee8d7afbe2fc987b9dc54
onetail <paosong91@gmail.com>
2021-03-07 23:38:55 +0800

    chore: createdAt updatedAt default and on update

--- app/model/base.go
+++ app/model/base.go
@@ -11,6 +11,6 @@ func (model *BaseModel) BeforeCreate(scope *gorm.Scope) error {
 }
 type BaseModel struct {
 	ID        string    `gorm:"type:char(36);primary_key"`
-	CreatedAt time.Time `gorm:"column:createdAt;index:idx_time"`
-	UpdatedAt time.Time `gorm:"column:updatedAt;index:idx_time"`
+	CreatedAt time.Time `gorm:"column:createdAt;index:idx_time;" sql:"DEFAULT:current_timestamp"`
+	UpdatedAt time.Time `gorm:"column:updatedAt;index:idx_time;" sql:"DEFAULT:current_timestamp ON update current_timestamp"`
 }
